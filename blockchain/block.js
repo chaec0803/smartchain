e93213eb5734e0a6c5310ea948c1690a820f7e1c
chaec0803 <chaec0803@gmail.com>
2025-07-19 17:21:01 +0900

    blockchain

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,17 +1,61 @@
-const {GENESIS_DATA} = require('../config')
+const { keccakHash } = require('../util');
+const { GENESIS_DATA } = require('../config')
 
-class Block{
-    constructor({blockHeaders}){
+const HASH_LENGTH = 64;
+const MAX_HASH_VALUE = parseInt('f'.repeat(HASH_LENGTH), 16);
+const MAX_NONCE_VALUE = 2 ** 64;
+
+class Block {
+    constructor({ blockHeaders }) {
         this.blockHeaders = blockHeaders
     }
 
-    static mineBlock({lastBlock}){
+    static calculateBlockTargetHash({ lastBlock }) {
+        const value = (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(16);
 
+        if (value.length > HASH_LENGTH) {
+            return 'f'.repeat(HASH_LENGTH);
+        }
+
+        return '0'.repeat(HASH_LENGTH - value.length) + value;
     }
 
-    static genesis(){
+    static mineBlock({ lastBlock, beneficiary }) {
+        const target = Block.calculateBlockTargetHash({ lastBlock });
+
+        let timestamp, truncatedBlockHeaders, header, nonce, underTargetHash;
+
+        do {
+            timestamp = Date.now();
+            truncatedBlockHeaders = {
+                parentHash: keccakHash(lastBlock.blockHeaders),
+                beneficiary,
+                difficulty: lastBlock.blockHeaders.difficulty + 1,
+                number: lastBlock.blockHeaders.number + 1,
+                timestamp,
+            };
+            header = keccakHash(truncatedBlockHeaders);
+            nonce = Math.floor(Math.random() * MAX_NONCE_VALUE) + 1;
+
+            underTargetHash = keccakHash(header + nonce);
+        } while (underTargetHash >= target);
+
+        return new this({
+            blockHeaders: { ...truncatedBlockHeaders, nonce }
+        })
+    }
+
+
+    static genesis() {
         return new this(GENESIS_DATA);
     }
 }
 
 module.exports = Block;
+
+
+/**Term	What It Is
+Current Block:	The block you're trying to mine now
+Target Hash:	The threshold value determined by difficulty
+Under Target Hash:	The hash of your current block header, 
+                    if it's less than the target */
---
+++ blockchain/block.test.js
@@ -0,0 +1,48 @@
+const { keccakHash } = require('../util');
+const Block = require('./block');
+
+describe('Block', () => {
+    describe('calculateBlockTargetHash()', () => {
+        it('calculates the maximum hash when the last block difficulty is 1', () => {
+            expect(
+                Block.calculateBlockTargetHash({ lastBlock: { blockHeaders: { difficulty: 1 } } })
+            ).toEqual('f'.repeat(64));
+        });
+
+        it('calculates a low hash when the last block difficulty is high', () => {
+            expect(
+                Block.calculateBlockTargetHash({ lastBlock: { blockHeaders: { difficulty: 500 } } })
+                < '1'
+            ).toBe(true);
+        });
+    });
+
+    describe('mineBlock()', () => {
+        let lastBlock, minedBlock;
+
+        beforeEach(() => {
+            lastBlock = Block.genesis();
+            minedBlock = Block.mineBlock({ lastBlock, beneficiary: 'beneficiary' });
+        });
+
+        it('mines a block', () => {
+            expect(minedBlock).toBeInstanceOf(Block);
+        });
+
+
+        it('mines a block that meets the proof of work requirement', () => {
+            const target = Block.calculateBlockTargetHash({ lastBlock });
+
+            const { blockHeaders } = minedBlock;
+            const { nonce } = blockHeaders;
+            const truncatedBlockHeaders = { ...blockHeaders };
+            delete truncatedBlockHeaders.nonce;
+
+            const header = keccakHash(truncatedBlockHeaders);
+
+            const underTargetHash = keccakHash(header + nonce);
+
+            expect(underTargetHash < target).toBe(true);
+        })
+    })
+})
