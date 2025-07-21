564a07ddd8379ea2f5c530d1016838e92a6b28c2
chaec0803 <chaec0803@gmail.com>
2025-07-21 18:13:17 +0900

    pubsub

--- api/pubsub.js
+++ api/pubsub.js
@@ -1,41 +1,41 @@
-const PubNub = require('pubnub');
+const PubNub = require("pubnub");
 
 const credentials = {
-    publishKey: 'pub-c-cb28fb52-fed6-457b-8b5a-bca5d0d34617',
-    subscribeKey: 'sub-c-483bf1a0-0249-48ce-abb3-1a5efe25615d',
-    secretKey: 'sec-c-ZDE3MTE0N2UtOWEwNi00NGRhLTk2OWMtZTcyODU5NzYzMzc0',
-    userId: 'blockchain-node-1'
+  publishKey: "pub-c-cb28fb52-fed6-457b-8b5a-bca5d0d34617",
+  subscribeKey: "sub-c-483bf1a0-0249-48ce-abb3-1a5efe25615d",
+  secretKey: "sec-c-ZDE3MTE0N2UtOWEwNi00NGRhLTk2OWMtZTcyODU5NzYzMzc0",
+  userId: "blockchain-node-1",
 };
 
 const CHANNELS_MAP = {
-    TEST: 'TEST',
-    BLOCK: 'BLOCK',
+  TEST: "TEST",
+  BLOCK: "BLOCK",
 };
 
 class PubSub {
-    constructor() {
-        this.pubnub = new PubNub(credentials);
-        this.subscribeToChannels();
-        this.listen();
-    }
+  constructor() {
+    this.pubnub = new PubNub(credentials);
+    this.subscribeToChannels();
+    this.listen();
+  }
 
-    subscribeToChannels() {
-        this.pubnub.subscribe({
-            channels: Object.values(CHANNELS_MAP)
-        })
-    }
+  subscribeToChannels() {
+    this.pubnub.subscribe({
+      channels: Object.values(CHANNELS_MAP),
+    });
+  }
 
-    publish({ channel, message }) {
-        this.pubnub.publish({ channel, message });
-    }
+  publish({ channel, message }) {
+    this.pubnub.publish({ channel, message });
+  }
 
-    listen() {
-        this.pubnub.addListener({
-            message: messageObject => {
-                console.log('messageObject', messageObject);
-            }
-        })
-    }
+  listen() {
+    this.pubnub.addListener({
+      message: (messageObject) => {
+        console.log("messageObject", messageObject);
+      },
+    });
+  }
 }
 
 module.exports = PubSub;
@@ -43,8 +43,8 @@ module.exports = PubSub;
 const pubsub = new PubSub();
 
 setTimeout(() => {
-    pubsub.publish({
-        channel: CHANNELS_MAP.TEST,
-        message: 'foo'
-    })
-}, 3000)
+  pubsub.publish({
+    channel: CHANNELS_MAP.TEST,
+    message: "foo",
+  });
+}, 3000);
